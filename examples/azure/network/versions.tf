584a2b84f9ec1539d4274334f95ebb24c9bd2eb0
Gabriel Ferreira <ferr3ira.gabriel@gmail.com>
2025-04-22 09:41:08 -0300

    fix(network): configure random provider

--- examples/azure/network/versions.tf
+++ examples/azure/network/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 3"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.0"
+      version = "~> 3"
     }
   }
 }
