81dbd267cc563db6b4f25377531d2c928b490c2b
LF Barrile <lf@lerian.studio>
2025-05-12 15:09:25 -0300

    fix: set the values of backend

--- examples/azure/base-resources/backend.tf
+++ examples/azure/base-resources/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "lerian-terraform-rg"
-    storage_account_name = "lerianterraformstate"
+    resource_group_name  = "lerian-terraform-rg" # Change for your resource group name
+    storage_account_name = "lerianterraformstate" #Change for your storage account name
     container_name       = "terraform-state"
     key                  = "azure/dns/terraform.tfstate"
   }
