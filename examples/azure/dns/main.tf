5589f4cd12253557bbd3523611c210ebb4d0cc99
LF Barrile <lf@lerian.studio>
2025-05-28 16:47:18 -0300

    feat: configure environment value with placeholder

--- examples/azure/dns/main.tf
+++ examples/azure/dns/main.tf
@@ -24,19 +24,3 @@ resource "azurerm_private_dns_zone_virtual_network_link" "main" {
     managed_by  = "terraform"
   }
 }
-
-# A records only
-resource "azurerm_private_dns_a_record" "records" {
-  for_each = { for record in var.a_records : record.name => record }
-
-  name                = each.value.name
-  zone_name           = azurerm_private_dns_zone.main.name
-  resource_group_name = data.azurerm_resource_group.dns.name
-  ttl                 = each.value.ttl
-  records             = each.value.records
-
-  tags = {
-    environment = var.environment
-    managed_by  = "terraform"
-  }
-}
--- examples/azure/dns/outputs.tf
+++ examples/azure/dns/outputs.tf
@@ -12,8 +12,3 @@ output "vnet_link_id" {
   description = "The ID of the Private DNS Zone Virtual Network Link"
   value       = azurerm_private_dns_zone_virtual_network_link.main.id
 }
-
-output "a_records" {
-  description = "Map of A records created"
-  value       = { for k, v in azurerm_private_dns_a_record.records : k => v.fqdn }
-}
