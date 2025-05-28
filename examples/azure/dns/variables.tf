8b6bf87bdd4ab92bf914cf8082d106bc4ac6f86f
LF Barrile <lf@lerian.studio>
2025-05-28 16:50:54 -0300

    feat: remove A records variable on DNS module

--- examples/azure/dns/variables.tf
+++ examples/azure/dns/variables.tf
@@ -12,19 +12,3 @@ variable "environment" {
   description = "Environment name for tagging"
   type        = string
 }
-
-variable "a_records" {
-  description = "List of A records to create"
-  type = list(object({
-    name    = string
-    ttl     = number
-    records = list(string)
-  }))
-  default = [
-    {
-      name    = "www"
-      ttl     = 3600
-      records = ["10.0.1.4", "10.0.1.5"]
-    }
-  ]
-}
