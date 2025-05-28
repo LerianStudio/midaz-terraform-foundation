274ac6c61f99986d42892abc539f3c7a6b1b9ea7
LF Barrile <lf@lerian.studio>
2025-05-28 15:52:35 -0300

    feat: summarize dns module to exclude unused resources

--- examples/azure/dns/main.tf
+++ examples/azure/dns/main.tf
@@ -25,7 +25,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "main" {
   }
 }
 
-# A records
+# A records only
 resource "azurerm_private_dns_a_record" "records" {
   for_each = { for record in var.a_records : record.name => record }
 
@@ -34,64 +34,6 @@ resource "azurerm_private_dns_a_record" "records" {
   resource_group_name = data.azurerm_resource_group.dns.name
   ttl                 = each.value.ttl
   records             = each.value.records
-
-  tags = {
-    environment = var.environment
-    managed_by  = "terraform"
-  }
-}
-
-# CNAME records
-resource "azurerm_private_dns_cname_record" "records" {
-  for_each = { for record in var.cname_records : record.name => record }
-
-  name                = each.value.name
-  zone_name           = azurerm_private_dns_zone.main.name
-  resource_group_name = data.azurerm_resource_group.dns.name
-  ttl                 = each.value.ttl
-  record              = each.value.record
-
-  tags = {
-    environment = var.environment
-    managed_by  = "terraform"
-  }
-}
-
-# MX records
-resource "azurerm_private_dns_mx_record" "records" {
-  for_each = { for record in var.mx_records : record.name => record }
-
-  name                = each.value.name
-  zone_name           = azurerm_private_dns_zone.main.name
-  resource_group_name = data.azurerm_resource_group.dns.name
-  ttl                 = each.value.ttl
-
-  dynamic "record" {
-    for_each = each.value.records
-    content {
-      preference = record.value.preference
-      exchange   = record.value.exchange
-    }
-  }
-
-  tags = {
-    environment = var.environment
-    managed_by  = "terraform"
-  }
-}
-
-# TXT records
-resource "azurerm_private_dns_txt_record" "records" {
-  for_each = { for record in var.txt_records : record.name => record }
-
-  name                = each.value.name
-  zone_name           = azurerm_private_dns_zone.main.name
-  resource_group_name = data.azurerm_resource_group.dns.name
-  ttl                 = each.value.ttl
-
-  record {
-    value = join(" ", each.value.records)
-  }
 
   tags = {
     environment = var.environment
