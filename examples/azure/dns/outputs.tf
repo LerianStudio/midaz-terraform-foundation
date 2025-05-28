60cd73fef9faf9a7480a7d14094e5aa5920785b7
LF Barrile <lf@lerian.studio>
2025-05-28 15:56:07 -0300

    feat: remove output entries

--- examples/azure/dns/outputs.tf
+++ examples/azure/dns/outputs.tf
@@ -17,18 +17,3 @@ output "a_records" {
   description = "Map of A records created"
   value       = { for k, v in azurerm_private_dns_a_record.records : k => v.fqdn }
 }
-
-output "cname_records" {
-  description = "Map of CNAME records created"
-  value       = { for k, v in azurerm_private_dns_cname_record.records : k => v.fqdn }
-}
-
-output "mx_records" {
-  description = "Map of MX records created"
-  value       = { for k, v in azurerm_private_dns_mx_record.records : k => v.fqdn }
-}
-
-output "txt_records" {
-  description = "Map of TXT records created"
-  value       = { for k, v in azurerm_private_dns_txt_record.records : k => v.fqdn }
-}
